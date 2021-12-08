228ca4a0a1cb38f26f5f1a1e7092d4f5ebdc85b5
YelovSK <hampatrik@gmail.com>
2021-12-08 13:16:05 +0100

    permutation solution

---
+++ 08/part2_perm.py
@@ -0,0 +1,37 @@
+from itertools import permutations
+
+with open("08\\input.txt") as f:
+    content = f.readlines()
+
+inputs = [line.split(" | ")[0].split() for line in content]
+outputs = [line.split(" | ")[1].split() for line in content]
+
+nums = {
+    "0": [0, 1, 2, 4, 5, 6],
+    "1": [2, 5],
+    "2": [0, 2, 3, 4, 6],
+    "3": [0, 2, 3, 5, 6],
+    "4": [1, 2, 3, 5],
+    "5": [0, 1, 3, 5, 6],
+    "6": [0, 1, 3, 4, 5, 6],
+    "7": [0, 2, 5],
+    "8": [0, 1, 2, 3, 4, 5, 6],
+    "9": [0, 1, 2, 3, 5, 6]
+}
+
+def get_nums(segments, nums):
+    return {
+        "".join(sorted([segments[ix] for ix in index_list])): key for key, index_list in nums.items()
+    }
+
+res = 0
+for input_line, output_line in zip(inputs, outputs):
+    input_line = {"".join(sorted(x)) for x in input_line}
+    output_line = ["".join(sorted(x)) for x in output_line]
+    for perm in permutations("abcdefg"):
+        curr_nums = get_nums(perm, nums)
+        if input_line == curr_nums.keys():
+            chars = "".join([curr_nums[x] for x in output_line])
+            res += int(chars)
+
+print(res)
