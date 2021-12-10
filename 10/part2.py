aa98a9041c83558ba279c91dc383a99e6f8aeaab
YelovSK <hampatrik@gmail.com>
2021-12-10 15:06:59 +0100

    better

--- 10/part2.py
+++ 10/part2.py
@@ -2,9 +2,9 @@ with open("10\\input.txt") as f:
     lines = [line.strip() for line in f.readlines()]
 
 pairs = {
+    "(": ")",
     "[": "]",
     "{": "}",
-    "(": ")",
     "<": ">"
 }
 
@@ -15,31 +15,18 @@ bracket_vals = {
     ">": 4
 }
 
-def validate_line(line):
-    opening = []
-    for char in line:
-        if char in pairs.keys():
-            opening.append(char)
-        elif char in bracket_vals.keys():
-            if pairs[opening.pop()] != char:
-                return char
-
 def score_incomplete(line):
     score = 0
     opening = []
     for char in line:
         if char in pairs.keys():
             opening.insert(0, char)
-        elif char in bracket_vals.keys():
-            opening.pop(0)
+        elif pairs[opening.pop(0)] != char:
+            return 0
     for char in opening:
         score *= 5
         score += bracket_vals[pairs[char]]
     return score
 
-corrupted_lines = [line for line in lines if validate_line(line)]
-for line in corrupted_lines:
-    lines.remove(line)
-
-scores = [score_incomplete(line) for line in lines]
-print(sorted(scores)[len(scores)//2])
+scores = [score for line in lines if (score := score_incomplete(line))] # uwu what's diz
+print(sorted(scores)[len(scores) // 2])
