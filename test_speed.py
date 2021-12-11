4a0267102c174d4f8fd4fbd6c73b5a904e0d8828
YelovSK <hampatrik@gmail.com>
2021-12-11 15:02:57 +0100

    nyoom

--- test_speed.py
+++ test_speed.py
@@ -4,19 +4,27 @@ folders = [filename for filename in os.listdir() if os.path.isdir(os.path.join(f
 
 times = [time.perf_counter()]
 for folder in folders:
-    runpy.run_path(f"{folder}\\part1.py")
+    if folder == "06":
+        runpy.run_path(f"{folder}\\part1_fast.py")
+    else:
+        runpy.run_path(f"{folder}\\part1.py")
     times.append(time.perf_counter())    
     if folder != "07":
         runpy.run_path(f"{folder}\\part2.py")
     else:
         runpy.run_path(f"{folder}\\part2_fast.py")
     times.append(time.perf_counter())
-    
+
+part1_total = 0
+part2_total = 0
 for i, folder in enumerate(folders):
     part1_time = '{:.4f}'.format(round(times[i*2+1]-times[i*2], 4))
     part2_time = '{:.4f}'.format(round(times[i*2+2]-times[i*2+1], 4))
+    part1_total += times[i*2+1]-times[i*2]
+    part2_total += times[i*2+2]-times[i*2+1]
     print(folder, end=" | ")
     print(f"part1 -> {part1_time}s", end=" | ")
     print(f"part2 -> {part2_time}s")
 
 print(f"Total time: {round(times[-1]-times[0], 4)}s")
+print(part1_total, part2_total)
