1efe3323f52ebc966c6bcd9cb3daefa22873ff6b
YelovSK <hampatrik@gmail.com>
2021-12-14 11:35:46 +0100

    what

--- 14/part2.py
+++ 14/part2.py
@@ -1,14 +1,14 @@
 from collections import defaultdict
 
 with open("14\\input.txt") as f:
-    lines = [line.strip() for line in f.readlines()]
+	lines = [line.strip() for line in f.readlines()]
 start = [x for x in lines[0]]
 pairs = dict(line.split(" -> ")  for line in lines[2:])
 
 pair_count = defaultdict(int)
 occurence_map = defaultdict(int)
 for x in start:
-    occurence_map[x] += 1
+	occurence_map[x] += 1
 for i in range(len(start)-1):
 	if start[i] + start[i+1] in pairs:
 		pair_count[start[i]+start[i+1]] += 1
