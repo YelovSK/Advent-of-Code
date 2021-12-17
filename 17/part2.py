edbf19910d06655c3c6596bce560b30b6ec877c6
YelovSK <hampatrik@gmail.com>
2021-12-17 10:49:11 +0100

    nyoom

--- 17/part2.py
+++ 17/part2.py
@@ -13,21 +13,25 @@ yrange = i_range(int(y1), int(y2))
 max_x = max(xrange)
 min_y = min(yrange)
 
-def shoot(dx, dy):
-    x, y = 0, 0
-    while x < max_x and y > min_y:
-        x += dx
-        y += dy
-        if x in xrange and y in yrange:
-            return True
-        if dx > 0:
-            dx -= 1
-        dy -= 1
-    return False
+def hit_count():    # shit is faster with PyPy if the loop is inside a function
 
-count = 0
-for dx in range(max_x+1):
-    for dy in range(min_y, 500):
-        if shoot(dx, dy):
-            count += 1
-print(count)
+    def shoot(dx, dy):
+        x, y = 0, 0
+        while x < max_x and y > min_y:
+            x += dx
+            y += dy
+            if x in xrange and y in yrange:
+                return True
+            if dx > 0:
+                dx -= 1
+            dy -= 1
+        return False
+
+    count = 0
+    for dx in range(max_x+1):
+        for dy in range(min_y, 500):
+            if shoot(dx, dy):
+                count += 1
+    return count
+
+print(hit_count())
