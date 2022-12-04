c3875f0d39cc7b2a8dd6143097b73623d96e5432
YelovSK <hampatrik@gmail.com>
2022-12-04 12:02:38 +0100

    add 2022/04

---
+++ 2022/04/part2.py
@@ -0,0 +1,31 @@
+from attr import dataclass
+
+
+@dataclass
+class Pair:
+    first: set[int]
+    second: set[int]
+
+    def is_full_overlap(self) -> bool:
+        intersection = set(self.first) & set(self.second)
+        return len(intersection) == len(self.first) or len(intersection) == len(self.second)
+
+    def is_overlap(self) -> bool:
+        return any(set(self.first) & set(self.second))
+
+
+with open("04/input.txt") as f:
+    data = [l.strip().split(",") for l in f.readlines()]
+
+pairs: list[Pair] = []
+for l, r in data:
+    left_start, left_end = map(int, l.split("-"))
+    right_start, right_end = map(int, r.split("-"))
+
+    left_set = set(range(left_start, left_end + 1))
+    right_set = set(range(right_start, right_end + 1))
+
+    pairs.append(Pair(left_set, right_set))
+
+overlap_count = sum([p.is_overlap() for p in pairs])
+print("Part 2:", overlap_count)
