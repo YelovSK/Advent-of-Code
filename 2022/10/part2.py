d694db0bc4c74250cb073936bca9dc391a963b9a
YelovSK <hampatrik@gmail.com>
2022-12-10 14:19:19 +0100

    weird

--- 2022/10/part2.py
+++ 2022/10/part2.py
@@ -1,45 +1,27 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class Instruction:
-    cycles: int
-    add: int
-
-
-def get_instruction() -> Instruction:
-    instruction_str = data.pop()
-    if instruction_str == "noop":
-        return Instruction(1, 0)
-    else:
-        number = int(instruction_str.split()[1])
-        return Instruction(2, number)
-
-
 def print_pixel():
-    if cycle - 1 <= x <= cycle + 1:
+    global cycle
+
+    if cycle != 0 and cycle % 40 == 0:
+        cycle = 0
+        print("")
+        print_pixel()
+    elif cycle - 1 <= x <= cycle + 1:
         print("#", end="")
     else:
         print(".", end="")
 
-
 with open("10/input.txt") as f:
-    data = [l.strip() for l in f.readlines()][::-1]
+    data = [l.strip() for l in f.readlines()]
 
-instruction = get_instruction()
 x = 1
-cycle = 1
+cycle = 0
 print_pixel()
 
-while len(data):
-    instruction.cycles -= 1
-    if instruction.cycles == 0:
-        x += instruction.add
-        instruction = get_instruction()
-
-    print_pixel()
+for line in data:
     cycle += 1
+    print_pixel()
 
-    if cycle % 40 == 0:
-        cycle = 0
-        print("")
+    if line != "noop":
+        x += int(line.split()[1])
+        cycle += 1
+        print_pixel()
