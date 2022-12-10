dc05cc12663d8c7e5a4eb1739c0d45fc3ea0f2bf
YelovSK <hampatrik@gmail.com>
2022-12-10 14:19:19 +0100

    weird

--- 2022/10/part1.py
+++ 2022/10/part1.py
@@ -1,38 +1,23 @@
-from dataclasses import dataclass
+def check():
+    global result
 
-
-@dataclass
-class Instruction:
-    cycles: int
-    add: int
-
-
-def get_instruction() -> Instruction:
-    instruction_str = data.pop()
-    if instruction_str == "noop":
-        return Instruction(1, 0)
-    else:
-        number = int(instruction_str.split()[1])
-        return Instruction(2, number)
-
+    if (cycle + 20) % 40 == 0:
+        result += x * cycle
 
 with open("10/input.txt") as f:
-    data = [l.strip() for l in f.readlines()][::-1]
+    data = [l.strip() for l in f.readlines()]
 
-instruction = get_instruction()
 x = 1
 cycle = 1
 result = 0
 
-while len(data):
-    instruction.cycles -= 1
-    if instruction.cycles == 0:
-        x += instruction.add
-        instruction = get_instruction()
-
+for line in data:
     cycle += 1
+    check()
 
-    if (cycle + 20) % 40 == 0:
-        result += x * cycle
+    if line != "noop":
+        x += int(line.split()[1])
+        cycle += 1
+        check()
 
 print("Part 1:", result)
