2e32e0735b34dcc0e93002b0f8a5d8aa7cfdf4b4
YelovSK <hampatrik@gmail.com>
2022-12-11 12:53:03 +0100

    add 2022/11

---
+++ 2022/11/part2.py
@@ -0,0 +1,96 @@
+import math
+from dataclasses import dataclass
+
+
+@dataclass
+class Test:
+    divisible: int
+    monkey_true_ix: int
+    monkey_false_ix: int
+    operation: any
+
+    def __post_init__(self):
+        _, op, b = self.operation.split()
+
+        if op == "+":
+            if b == "old":
+                self.operation = lambda item: item + item
+            else:
+                self.b = int(b)
+                self.operation = lambda item: item + self.b
+        elif op == "*":
+            if b == "old":
+                self.operation = lambda item: item * item
+            else:
+                self.b = int(b)
+                self.operation = lambda item: item * self.b
+
+    def get_monkey_true(self):
+        return monkeys[self.monkey_true_ix]
+
+    def get_monkey_false(self):
+        return monkeys[self.monkey_false_ix]
+
+
+class Monkey:
+
+    def __init__(self, items: list[int], divisibility: int, test: Test) -> None:
+        self.inspections = 0
+
+        self.items = items
+        self.divisibility = divisibility
+        self.test = test
+
+    def inspect(self):
+        self.inspections += len(self.items)
+        while self.items:
+            item = self.items.pop(0)
+
+            # 1. Operation
+            item = self.test.operation(item)
+
+            # 2. Check divisibility
+            true = item % self.divisibility == 0
+
+            # 3. We don't like big numbers
+            item %= mod
+
+            # 4. Move to another monkey
+            if true:
+                self.test.get_monkey_true().items.append(item)
+            else:
+                self.test.get_monkey_false().items.append(item)
+
+
+with open("11/input.txt") as f:
+    data = f.read().split("\n\n")
+
+monkeys = []
+mod = 1
+for monkey_string in data:
+    # Split by lines
+    monkey, items, operation, test, true_ix, false_ix = monkey_string.split("\n")
+
+    # List of ints representing items
+    items = [int(item) for item in items.split(": ")[1].split(", ")]
+
+    # X *+ Y
+    operation = operation.split("new = ")[1]
+
+    # Divisible by X
+    divisibility = int(test.split()[-1])
+    # Product of all tests
+    mod *= divisibility
+
+    # Monkey X if test passed, Monkey Y if not
+    true_ix = int(true_ix.split()[-1])
+    false_ix = int(false_ix.split()[-1])
+
+    monkeys.append(Monkey(items, divisibility, Test(divisibility, true_ix, false_ix, operation)))
+
+for _ in range(10_000):
+    for monkey in monkeys:
+        monkey.inspect()
+
+inspections = sorted([monkey.inspections for monkey in monkeys], reverse=True)
+print("Part 2:", math.prod(inspections[:2]))
--- 2022/README.md
+++ 2022/README.md
@@ -1,6 +1,6 @@
 # [Advent of Code 2022](https://adventofcode.com/2022)
 
-![](https://img.shields.io/badge/stars%20⭐-20-yellow)
+![](https://img.shields.io/badge/stars%20⭐-22-yellow)
 
 ### PROGRESS
 
@@ -16,3 +16,4 @@
 | 08 |   [Treetop Tree House](https://adventofcode.com/2022/day/8)    | [08](08) | ★★ |
 | 09 |       [Rope Bridge](https://adventofcode.com/2022/day/9)       | [09](09) | ★★ |
 | 10 |  [Cathode-Ray Tube](https://adventofcode.com/2022/day/10)      | [10](10) | ★★ |
+| 11 | [Monkey in the Middle](https://adventofcode.com/2022/day/11)   | [11](11) | ★★ |
