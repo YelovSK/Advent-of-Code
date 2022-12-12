341ba137c70bd8b5dc07d2de8156567724171ff2
YelovSK <hampatrik@gmail.com>
2022-12-12 18:01:04 +0100

    a

--- 2022/12/part1.py
+++ 2022/12/part1.py
@@ -1,7 +1,11 @@
 import collections
 
 
-def bfs(grid: dict[tuple[int, int], str], start: tuple[int, int], end: tuple[int, int]) -> list[tuple[int, int]]:
+def bfs(grid: list[list[int]],
+        start: tuple[int, int],
+        end: tuple[int, int]
+        ) -> list[tuple[int, int]]:
+
     queue = collections.deque([[start]])
     seen = set(start)
     paths = []
@@ -9,18 +13,18 @@ def bfs(grid: dict[tuple[int, int], str], start: tuple[int, int], end: tuple[int
     while queue:
         path = queue.popleft()
         x, y = path[-1]
-        curr_value = grid[(x, y)]
+        curr_value = grid[x][y]
 
         if (x, y) == end:
             paths.append(path)
 
-        for x2, y2 in ((x + 1,y), (x - 1, y), (x, y + 1), (x, y - 1)):
-            if x2 < 0 or y2 < 0 or x2 >= HEIGHT or y2 >= WIDTH:
+        for x2, y2 in ((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)):
+            if not (0 <= x2 < ROWS and 0 <= y2 < COLS):
                 continue
 
-            next_value = grid[(x2, y2)]
+            next_value = grid[x2][y2]
 
-            if (x2, y2) not in seen and ord(next_value) <= ord(curr_value) + 1:
+            if (x2, y2) not in seen and next_value <= curr_value + 1:
                 queue.append(path + [(x2, y2)])
                 seen.add((x2, y2))
     
@@ -29,10 +33,10 @@ def bfs(grid: dict[tuple[int, int], str], start: tuple[int, int], end: tuple[int
 with open("12/input.txt") as f:
     data = [l.strip() for l in f.readlines()]
 
-HEIGHT = len(data)
-WIDTH = len(data[0])
+ROWS = len(data)
+COLS = len(data[0])
 
-grid_map = {}
+grid = [[None] * COLS for _ in range(ROWS)]
 for row_ix, line in enumerate(data):
     for col_ix, c in enumerate(line):
         if c == "S":
@@ -41,8 +45,8 @@ for row_ix, line in enumerate(data):
         elif c == "E":
             end = (row_ix, col_ix)
             c = "z"
-        grid_map[(row_ix, col_ix)] = c
+        grid[row_ix][col_ix] = ord(c)
 
-paths = bfs(grid_map, start, end)
+paths = bfs(grid, start, end)
 shortest = min(paths, key=lambda x: len(x))
-print("Part 1:", len(shortest) - 1)
+print("Part 2:", len(shortest) - 1)
