d7be57ef88125fe670de56d05dc2fe29341611d2
YelovSK <hampatrik@gmail.com>
2023-12-06 13:25:06 +0100

    2023 06

---
+++ 2023/05/part1.py
@@ -0,0 +1,2 @@
+with open("2023/05/test_input.txt") as f:
+    data = f.read().strip()
---
+++ 2023/06/part1.py
@@ -0,0 +1,21 @@
+import cmath
+import math
+
+with open("2023/06/input.txt") as f:
+    lines = f.read().splitlines()
+
+times = [int(x) for x in lines[0].split()[1:]]
+distances = [int(x) for x in lines[1].split()[1:]]
+
+result = 1
+
+for time, distance in zip(times, distances):
+    # -(run_time**2) + (race_time*run_time) - distance = 0
+    a, b, c = -1, time, -distance
+    d = (b ** 2) - (4 * a * c)
+    max = (-b - cmath.sqrt(d)) / (2 * a)
+    min = (-b + cmath.sqrt(d)) / (2 * a)
+
+    result *= math.ceil(max.real - 1) - math.floor(min.real + 1) + 1
+
+print("Part 1", result)
