ae0163acc5422bf71bd6e8efa6bbefb9b155cd0f
YelovSK <hampatrik@gmail.com>
2023-12-06 13:34:30 +0100

    pointless cmath

--- 2023/06/part2.py
+++ 2023/06/part2.py
@@ -1,4 +1,3 @@
-import cmath
 import math
 
 with open("2023/06/input.txt") as f:
@@ -10,8 +9,8 @@ distance = int("".join(lines[1].split()[1:]))
 # -(run_time**2) + (race_time*run_time) - distance = 0
 a, b, c = -1, time, -distance
 d = (b ** 2) - (4 * a * c)
-max = (-b - cmath.sqrt(d)) / (2 * a)
-min = (-b + cmath.sqrt(d)) / (2 * a)
-result = math.ceil(max.real - 1) - math.floor(min.real + 1) + 1
+max = (-b - math.sqrt(d)) / (2 * a)
+min = (-b + math.sqrt(d)) / (2 * a)
+result = math.ceil(max - 1) - math.floor(min + 1) + 1
 
 print("Part 2", result)
