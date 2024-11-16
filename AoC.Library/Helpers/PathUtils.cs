fa0c744ca28ab9f34bf8a3480f7b3866369fa353
YelovSK <hampatrik@gmail.com>
2024-11-16 16:25:42 +0100

    c# 2024 base lib

---
+++ AoC.Library/Helpers/PathUtils.cs
@@ -0,0 +1,16 @@
+﻿namespace AoC.Library.Helpers;
+
+public static class PathUtils
+{
+    public static string GetBaseDirectory()
+    {
+        var currentDir = AppContext.BaseDirectory;
+
+        var baseDir = Directory.GetParent(currentDir);
+        baseDir = baseDir.Parent;
+        baseDir = baseDir.Parent;
+        baseDir = baseDir.Parent;
+
+        return baseDir.FullName;
+    }
+}
---
+++ AoC.Library/Solution.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+
+namespace AoC.Library;
+
+public class Solution
+{
+   public TimeSpan Part1Elapsed { get; set; } = TimeSpan.Zero;
+   public TimeSpan Part2Elapsed { get; set; } = TimeSpan.Zero;
+
+   public string? Part1Result { get; set; }
+   public string? Part2Result { get; set; }
+   
+   public TimeSpan Elapsed => Part1Elapsed.Add(Part2Elapsed);
+   
+   public override string ToString()
+   {
+       var builder = new StringBuilder();
+
+       if (Part1Result != null)
+       {
+           builder.AppendLine($"PART 1 in {Part2Elapsed.Milliseconds}ms:");
+           builder.AppendLine(Part1Result);
+       }
+       
+       if (Part2Result != null)
+       {
+           builder.AppendLine($"PART 2 in {Part1Elapsed.Milliseconds}ms:");
+           builder.AppendLine(Part2Result);
+       }
+       
+       return builder.ToString();
+   }
+}
+
+public class DaySolution(int day, Solution solution)
+{
+   public int Day { get; set; } = day;
+   public Solution Solution { get; set; } = solution;
+}
