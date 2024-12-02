5c3ecf45b346b6170bb007a00c92d300f397eefd
YelovSK <hampatrik@gmail.com>
2024-12-02 20:38:37 +0100

    move CLI stuff to library

---
+++ AoC.Library/CLI/CreateOptions.cs
@@ -0,0 +1,11 @@
+﻿using CommandLine;
+
+namespace AoC.Library.CLI
+{
+    [Verb("create", HelpText = "Create a folder for the current or specified day.")]
+    public class CreateOptions
+    {
+        [Option('d', "day", HelpText = "Day to create. Defaults to today's day.")]
+        public int? Day { get; set; }
+    }
+}
